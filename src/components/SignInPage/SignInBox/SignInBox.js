cd85bfdade8bc2da8ae790ba62c74db916d35f27
Chris McKnight <tenlongfingers10@gmail.com>
2022-10-17 18:22:27 -0600

    working on sign in box

--- src/components/SignInPage/SignInBox/SignInBox.js
+++ src/components/SignInPage/SignInBox/SignInBox.js
@@ -16,44 +16,64 @@ const SignInBox = () => {
   }, []);
 
   useEffect(() => {
-    userRef.current.focus();
+    setErrMsg("");
   }, [user, pwd]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  };
+
   return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}{" "}
-      </p>
-      <h1>Sign In</h1>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPwd(e.target.value)}
-          value={pwd}
-          required
-        />
-        <button>Sign in</button>
-      </form>
-      <p>Just want to test out the app?</p>
-      <p> Username: Friend </p>
-      <p>Password: Test</p>
-    </section>
+    <>
+      {success ? (
+        <section>
+          <h1> You are logged in!</h1>
+          <p>
+            <a href="homepage"> Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}{" "}
+          </p>
+
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign in</button>
+          </form>
+          <p>Just want to test out the app?</p>
+          <p> Username: Friend </p>
+          <p>Password: Test</p>
+        </section>
+      )}
+    </>
   );
 };
 
