9d1e33c126c3be9c1202c63c3f66511ca3ef642f
Chris McKnight <tenlongfingers10@gmail.com>
2022-11-05 12:45:02 -0600

    dialect options

--- server/serverIndex.js
+++ server/serverIndex.js
@@ -1,16 +1,24 @@
 //IMPORTS
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { Sequelize } = require("sequelize");
 
 const app = express();
 
-const PORT = 5000;
+const { PORT, CONNECTION_STRING } = process.env;
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnathorized: false,
+    },
+  },
+});
 
 //ENDPOINTS
