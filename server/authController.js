12df7a619643a46dce7ef7f38db95068bd409bd3
Chris McKnight <tenlongfingers10@gmail.com>
2022-11-15 18:33:31 -0700

    still struggling with connecting signinbox with authcontroller

--- server/authController.js
+++ server/authController.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 module.exports = {
   //SIGN IN
   login: () => {
-    console.log("login");
+    console.log("login"); //to get the whole database, use sequelize.query and then use SQL to select all
+    //Also, Stuart's video at 30:50 talks about inserting using async and await, so you can use that for when you start registering users maybe?
   },
 };
